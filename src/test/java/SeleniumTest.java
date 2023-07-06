29e9bf806b70b56dbcf76eb42d3cffff0920096f
LiquidPlummer <kyle.plummer@revature.com>
2023-07-06 17:36:17 -0400

    config and chromedriver

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -9,65 +9,76 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class SeleniumTest {
 
-    private WebDriver driver;
+    private WebDriver webDriver;
 
     @Before
     public void setUp() {
         // Set up ChromeDriver path
-        System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/driver/chromedriver");//linux_64
+
+        // Get file
+        File file = new File("HelloUser.html");
+        String path = "file://" + file.getAbsolutePath();
 
         // Create a new ChromeDriver instance
-        driver = new ChromeDriver();
-        File file = new File("HelloUser.html");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        webDriver = new ChromeDriver(options);
+
         // Open the HTML file
-        driver.get(file.getAbsolutePath());
+        webDriver.get(path);
+
+
+
+
     }
     @Test
     public void testNoName() {
         // Find the submit button and click it
-        WebElement submitButton = driver.findElement(By.id("submit"));
+        WebElement submitButton = webDriver.findElement(By.id("submit"));
         submitButton.click();
 
         // Find the text element and verify the greeting
-        WebElement displayElement = driver.findElement(By.id("text"));
+        WebElement displayElement = webDriver.findElement(By.id("text"));
         assertEquals("Hello,", displayElement.getText());
     }
 
     @Test
     public void testShortName() {
         // fill in the input box with a name:
-        WebElement inputBox = driver.findElement(By.id("input"));
+        WebElement inputBox = webDriver.findElement(By.id("input"));
         inputBox.sendKeys("Rick");
 
         // Find the submit button and click it
-        WebElement submitButton = driver.findElement(By.id("submit"));
+        WebElement submitButton = webDriver.findElement(By.id("submit"));
         submitButton.click();
 
         // Find the text element and verify the greeting
-        WebElement displayElement = driver.findElement(By.id("text"));
+        WebElement displayElement = webDriver.findElement(By.id("text"));
         assertEquals("Hello, Rick", displayElement.getText());
     }
 
     @Test
     public void testLongName() {
         // fill in the input box with a name:
-        WebElement inputBox = driver.findElement(By.id("input"));
+        WebElement inputBox = webDriver.findElement(By.id("input"));
         inputBox.sendKeys("Alexander Sebastian Montgomery Fitzgerald-Jenkins");
 
         // Find the submit button and click it
-        WebElement submitButton = driver.findElement(By.id("submit"));
+        WebElement submitButton = webDriver.findElement(By.id("submit"));
         submitButton.click();
 
         // Find the text element and verify the greeting
-        WebElement displayElement = driver.findElement(By.id("text"));
+        WebElement displayElement = webDriver.findElement(By.id("text"));
         assertEquals("Hello, Alexander Sebastian Montgomery Fitzgerald-Jenkins", displayElement.getText());
     }
     @After
     public void tearDown() {
         // Close the browser
-        driver.quit();
+        webDriver.quit();
     }
 }
